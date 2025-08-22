7f4b894c95a92a9355611521161178d816e8805e
Jean-Christophe Amiel <jeanchristophe.amiel@gmail.com>
2025-08-22 17:51:40 +0200

    Update CI for windows

--- benchmark.ps1
+++ benchmark.ps1
@@ -1,12 +1,12 @@
 
 
 Write-Host "Running pretty (iter=500):"
-Measure-Command { Get-Content 5mb.json -Raw | .\target\release\pretty.exe --iter 500 | Out-Null }
+Measure-Command { .\target\release\pretty.exe --iter 500 < 5mb.json | Out-Null }
 
 Write-Host ""
 Write-Host "Running pretty --no-color (iter=500):"
-Measure-Command { Get-Content 5mb.json -Raw | .\target\release\pretty.exe --iter 500 --no-color | Out-Null }
+Measure-Command { .\target\release\pretty.exe --iter 500 --no-color < 5mb.json | Out-Null }
 
 Write-Host ""
 Write-Host "Running pretty pretty --serde (iter=500):"
-Measure-Command { Get-Content 5mb.json -Raw | .\target\release\pretty.exe --iter 500 --serde | Out-Null }
+Measure-Command { .\target\release\pretty.exe --iter 500 --serde < 5mb.json | Out-Null }
