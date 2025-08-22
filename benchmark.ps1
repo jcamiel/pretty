8a80fec862673d1c1172ad47eca14fa9b4e7bf00
Jean-Christophe Amiel <jeanchristophe.amiel@gmail.com>
2025-08-22 17:54:53 +0200

    Update CI for windows

--- benchmark.ps1
+++ benchmark.ps1
@@ -1,12 +1,12 @@
 
 
 Write-Host "Running pretty (iter=500):"
-Measure-Command { .\target\release\pretty.exe --iter 500 < 5mb.json | Out-Null }
+Measure-Command { [System.IO.File]::OpenRead("5mb.json") | .\target\release\pretty.exe --iter 500 | Out-Null }
 
 Write-Host ""
 Write-Host "Running pretty --no-color (iter=500):"
-Measure-Command { .\target\release\pretty.exe --iter 500 --no-color < 5mb.json | Out-Null }
+Measure-Command { [System.IO.File]::OpenRead("5mb.json") | .\target\release\pretty.exe --iter 500 --no-color | Out-Null }
 
 Write-Host ""
 Write-Host "Running pretty pretty --serde (iter=500):"
-Measure-Command { .\target\release\pretty.exe --iter 500 --serde < 5mb.json | Out-Null }
+Measure-Command { [System.IO.File]::OpenRead("5mb.json") | .\target\release\pretty.exe --iter 500 --serde | Out-Null }
