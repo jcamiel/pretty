b45a570ccab65403f15d95fa59f0beff64c3c9ff
Jean-Christophe Amiel <jeanchristophe.amiel@gmail.com>
2025-08-23 23:46:02 +0200

    Adapt benchmark for windows

--- benchmark.ps1
+++ benchmark.ps1
@@ -1,13 +1,16 @@
 $iter = [int]$args[0]
+$file = $args[1]
 
-
-Write-Host "Running pretty (iter=$iter):"
-Measure-Command { .\target\release\pretty.exe --iter $iter 5mb.json | Out-Null }
-
-Write-Host ""
-Write-Host "Running pretty --no-color (iter=$iter):"
-Measure-Command { .\target\release\pretty.exe --iter $iter --no-color 5mb.json | Out-Null }
+Write-Host "Benchmark iter=$iter file=$file"
 
 Write-Host ""
-Write-Host "Running pretty --serde (iter=$iter):"
-Measure-Command { .\target\release\pretty.exe --iter $iter --serde 5mb.json | Out-Null }
+Write-Host "Running pretty:"
+Measure-Command { .\target\release\pretty.exe --iter $iter $file | Out-Null }
+
+Write-Host ""
+Write-Host "Running pretty --no-color:"
+Measure-Command { .\target\release\pretty.exe --iter $iter --no-color $file | Out-Null }
+
+Write-Host ""
+Write-Host "Running pretty --serde:"
+Measure-Command { .\target\release\pretty.exe --iter $iter --serde $file | Out-Null }
